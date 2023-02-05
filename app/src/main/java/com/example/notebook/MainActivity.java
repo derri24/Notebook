06f505fe7aadd15c3baa6d797e3105b9ed8dd4e3
tranquility09 <derri5437@gmail.com>
2023-02-05 15:32:42 +0300

    update

--- app/src/main/java/com/example/notebook/MainActivity.java
+++ app/src/main/java/com/example/notebook/MainActivity.java
@@ -1,21 +1,15 @@
 package com.example.notebook;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,94 +17,113 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         connect();
-        //GetData();
     }
-
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        GetData();
-    }
-
-    public SQLiteDatabase db;
-
-    public void GetData() {
-        // создание TextView
-
-        LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
-
-        layout.removeAllViews();
-        Cursor query = db.rawQuery("SELECT id,name,note FROM data;", null);
-        int i = 0;
-        while (query.moveToNext()) {
-
-
-            int idM = query.getInt(0);
-            String name = query.getString(1);
-            String note = query.getString(2);
-            TextView textView = new TextView(this);
-            textView.setText("id:" + idM + " name: " + name + " note: " + note);
-            textView.setId(i);
-            textView.setTextSize(18);
-            textView.setTop(50);
-            textView.setEms(13);
-
-            Button deleteBtn = new Button(this);
-            deleteBtn.setId(i);
-            deleteBtn.setText("⌫");
-            deleteBtn.setWidth(50);
-            deleteBtn.setHeight(50);
-            deleteBtn.setTop(50);
-            deleteBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    try{
-                        db.execSQL(" DELETE FROM data WHERE id ="+ idM+";");
-                        GetData();
-                    }
-                    catch(Exception ex){
-
-                    }
-
-                }
-            });
-
-            Button editBtn = new Button(this);
-            editBtn.setId(i);
-            editBtn.setText("✏️");
-            // editBtn.set(50);
-            editBtn.setHeight(50);
-            editBtn.setTop(50);
-
-            editBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    NoteActivity.flag ="UPDATE";
-                    NoteActivity.id=idM;
-                    createNote(view);
-                }
-            });
-
-
-            //setContentView(scrollView);
-            layout.addView(textView);
-            layout.addView(deleteBtn);
-            layout.addView(editBtn);
-            i++;
-        }
-    }
-
-    private void connect() {
-        db = getBaseContext().openOrCreateDatabase("application.db", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY,name TEXT, note Text)");
+        getData();
     }
 
     public void createNote(View view) {
         Intent intent = new Intent(this, NoteActivity.class);
+        NoteActivity.setActivity("CREATE");
         startActivity(intent);
     }
+
+    public void updateNote() {
+        Intent intent = new Intent(this, NoteActivity.class);
+        NoteActivity.setActivity("UPDATE");
+        startActivity(intent);
+    }
+
+    private SQLiteDatabase db;
+
+    private void connect() {
+        try {
+            db = getBaseContext().openOrCreateDatabase("application.db", MODE_PRIVATE, null);
+            db.execSQL("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY,name TEXT, note Text)");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void getData() {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
+        layout.removeAllViews();
+        try {
+            Cursor query = db.rawQuery("SELECT id,name,note FROM data;", null);
+            int i = 0;
+            while (query.moveToNext()) {
+                int id = query.getInt(0);
+                String name = query.getString(1);
+
+                TextView textView = createTextView(i, name);
+                Button deleteBtn = createDeleteButton(i, id);
+                Button updateBtn = createUpdateButton(i, id);
+
+                layout.addView(textView);
+                layout.addView(updateBtn);
+                layout.addView(deleteBtn);
+                i++;
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private TextView createTextView(int i, String name) {
+        TextView textView = new TextView(this);
+        textView.setText(name);
+        textView.setId(i);
+        textView.setTextSize(18);
+        textView.setTop(50);
+        textView.setEms(13);
+        return textView;
+    }
+
+    private Button createDeleteButton(int i, int id) {
+        Button deleteBtn = new Button(this);
+        deleteBtn.setId(i);
+        deleteBtn.setText("⌫");
+        deleteBtn.setWidth(50);
+        deleteBtn.setHeight(50);
+        deleteBtn.setTop(50);
+        deleteBtn.setLeft(500);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    db.execSQL(" DELETE FROM data WHERE id =" + id + ";");
+                    getData();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        });
+        return deleteBtn;
+    }
+
+    private Button createUpdateButton(int i, int id) {
+        Button updateBtn = new Button(this);
+        updateBtn.setId(i);
+        updateBtn.setText("✏️");
+        updateBtn.setHeight(50);
+        updateBtn.setTop(50);
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NoteActivity.setId(id);
+                updateNote();
+            }
+        });
+        return updateBtn;
+    }
+
+
 }
--- app/src/main/java/com/example/notebook/NoteActivity.java
+++ app/src/main/java/com/example/notebook/NoteActivity.java
@@ -7,7 +7,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.sql.Connection;
@@ -17,12 +19,17 @@ import java.sql.SQLException;
 import java.util.Objects;
 
 public class NoteActivity extends AppCompatActivity {
+    private static String activity;
 
-    public SQLiteDatabase db;
+    public static void setActivity(String _activity) {
+        activity = _activity;
+    }
 
+    private static int id;
 
-    public static String flag;
-    public static int id;
+    public static void setId(int _id) {
+        id = _id;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,25 +38,50 @@ public class NoteActivity extends AppCompatActivity {
         connect();
     }
 
-
     @Override
     protected void onStart() {
         super.onStart();
-
+        if (Objects.equals(activity, "UPDATE"))
+            getData();
     }
+
+    private void getData() {
+
+        Cursor query = db.rawQuery("SELECT name,note FROM data WHERE id=" + id + ";", null);
+        query.moveToNext();
+        String name = query.getString(0);
+        String note = query.getString(1);
+
+        ((EditText) findViewById(R.id.nameBox)).setText(name);
+        ((EditText) findViewById(R.id.noteBox)).setText(note);
+    }
+
+
+    private SQLiteDatabase db;
 
     private void connect() {
-        db = getBaseContext().openOrCreateDatabase("application.db", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY,name TEXT, note Text)");
+        try {
+            db = getBaseContext().openOrCreateDatabase("application.db", MODE_PRIVATE, null);
+            db.execSQL("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY,name TEXT, note Text)");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-
-    public void insert(String name, String content) {
-        db.execSQL("INSERT OR IGNORE INTO data(name,note) VALUES ('" + name + "','" + content + "');");
+    private void insert(String name, String note) {
+        try {
+            db.execSQL("INSERT OR IGNORE INTO data(name,note) VALUES ('" + name + "','" + note + "');");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void update() {
-
+    private void update(String name, String note) {
+        try {
+            db.execSQL("UPDATE data SET note ='" + note + "', name = '" + name + "' WHERE id =" + id + ";");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveNote(View view) {
@@ -57,16 +89,12 @@ public class NoteActivity extends AppCompatActivity {
             String name = ((EditText) findViewById(R.id.nameBox)).getText().toString();
             String note = ((EditText) findViewById(R.id.noteBox)).getText().toString();
 
-            if (Objects.equals(flag, "UPDATE")) {
-
-                db.execSQL("UPDATE data SET note ='" + note + "', name = '" + name + "' WHERE id =" + id + ";");
-                finish();
+            if (Objects.equals(activity, "CREATE")) {
+                insert(name, note);
+            } else if (Objects.equals(activity, "UPDATE")) {
+                update(name, note);
             }
-
-            insert(name, note);
-
             finish();
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
